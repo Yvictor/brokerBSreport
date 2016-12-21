2f35c91866649ef0b5b0d949beb9dbb57d177c5f
Yvictor <fate2314@gmail.com>
2016-12-21 23:42:32 +0800

    print done info

--- twse/StockId.py
+++ twse/StockId.py
@@ -12,6 +12,7 @@ def _get_twse_stock_id():
     res = requests.get('http://isin.twse.com.tw/isin/C_public.jsp?strMode=2')
     twse_no = pd.read_html(res.text, header=0)[0]
     twse_no = twse_no[twse_no['CFICode']=='ESVUFR']
+    print('Done')
     return twse_no.iloc[:, 0].map(lambda x: x.split(' ')[0].split('\u3000')[0]).tolist()
 
 if __name__ == '__main__':
